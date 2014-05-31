0721537f762d5f1a8fe2bdef45fc7a1b1ec9f578
izrik <izrik@izrik.com>
2014-05-31 15:10:17 -0500

    Cache adapters for Texture2D and SpriteFont.

--- ChamberLib/SpriteFontAdapter.cs
+++ ChamberLib/SpriteFontAdapter.cs
@@ -1,15 +1,31 @@
 ﻿using System;
+using SpriteFont = Microsoft.Xna.Framework.Graphics.SpriteFont;
+using System.Collections.Generic;
 
 namespace ChamberLib
 {
     public class SpriteFontAdapter : IFont
     {
-        public SpriteFontAdapter(Microsoft.Xna.Framework.Graphics.SpriteFont font)
+        protected static readonly Dictionary<SpriteFont, SpriteFontAdapter> _cache = new Dictionary<SpriteFont, SpriteFontAdapter>();
+
+        public static IFont GetAdapter(SpriteFont font)
+        {
+            if (_cache.ContainsKey(font))
+            {
+                return _cache[font];
+            }
+
+            var adapter = new SpriteFontAdapter(font);
+            _cache[font] = adapter;
+            return adapter;
+        }
+
+        protected SpriteFontAdapter(SpriteFont font)
         {
             SpriteFont = font;
         }
 
-        public Microsoft.Xna.Framework.Graphics.SpriteFont SpriteFont;
+        public SpriteFont SpriteFont;
 
         public Vector2 MeasureString(string text)
         {
--- ChamberLib/Texture2DAdapter.cs
+++ ChamberLib/Texture2DAdapter.cs
@@ -1,15 +1,31 @@
 ﻿using System;
+using Texture2D = Microsoft.Xna.Framework.Graphics.Texture2D;
+using System.Collections.Generic;
 
 namespace ChamberLib
 {
     public class Texture2DAdapter : ITexture2D
     {
-        public Texture2DAdapter(Microsoft.Xna.Framework.Graphics.Texture2D texture)
+        protected static readonly Dictionary<Texture2D, Texture2DAdapter> _cache = new Dictionary<Texture2D, Texture2DAdapter>();
+
+        public static ITexture2D GetAdapter(Texture2D font)
+        {
+            if (_cache.ContainsKey(font))
+            {
+                return _cache[font];
+            }
+
+            var adapter = new Texture2DAdapter(font);
+            _cache[font] = adapter;
+            return adapter;
+        }
+
+        protected Texture2DAdapter(Texture2D texture)
         {
             Texture = texture;
         }
 
-        public Microsoft.Xna.Framework.Graphics.Texture2D Texture;
+        public Texture2D Texture;
 
         public Vector2 GetSize()
         {
