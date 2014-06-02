3867c57667726cad34901603292ade76fde442e4
izrik <izrik@izrik.com>
2014-06-02 08:13:19 -0500

    Move half of the Collection class to ChamberLib.

---
+++ ChamberLib/Collection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChamberLib
+{
+    public static class Collection
+    {
+        public static void AddRange<T>(ICollection<T> collection, IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                collection.Add(item);
+            }
+        }
+
+        public static void RemoveRange<T>(ICollection<T> collection, IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                collection.Remove(item);
+            }
+        }
+
+        public static void AddRange<T, U>(this ICollection<T> collection, IEnumerable<U> items)
+            where U : T
+        {
+            foreach (U item in items)
+            {
+                collection.Add(item);
+            }
+        }
+        public static void RemoveRange<T, U>(this ICollection<T> collection, IEnumerable<U> items)
+            where U : T
+        {
+            foreach (U item in items)
+            {
+                collection.Remove(item);
+            }
+        }
+        public static void RemoveKeys<TKey, TValue, U>(this IDictionary<TKey, TValue> dictionary, IEnumerable<U> keys)
+            where U : TKey
+        {
+            foreach (U key in keys)
+            {
+                dictionary.Remove(key);
+            }
+        }
+        public static void EnqueueRange<T>(this Queue<T> queue, IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                queue.Enqueue(item);
+            }
+        }
+    }
+}
