eb30b8fcf3fa52899897003510535de5513ecfc3
izrik <izrik@izrik.com>
2014-06-02 12:54:08 -0500

    Extract the EnableDefaultLighting, DisableDirectionalLight, SetAlpha, SetTexture, SetWorldViewProjection, and SetBoneTransforms methods into IModel/ModelAdapter.

--- ChamberLib/IModel.cs
+++ ChamberLib/IModel.cs
@@ -13,10 +13,20 @@ namespace ChamberLib
 
         IBone Root { get; set; }
 
+        void EnableDefaultLighting();
+
         void SetAmbientLightColor(Vector3 value);
         void SetEmissiveColor(Vector3 value);
-
         void SetDirectionalLight(DirectionalLight light, int index=0);
+        void DisableDirectionalLight(int index);
+
+        void SetAlpha(float alpha);
+
+        void SetTexture(ITexture2D texture);
+
+        void SetWorldViewProjection(Matrix transform, Matrix view, Matrix projection);
+
+        void SetBoneTransforms(Matrix[] boneTransforms, float verticalOffset, Matrix world);
     }
 }
 
--- ChamberLib/ModelAdapter.cs
+++ ChamberLib/ModelAdapter.cs
@@ -4,6 +4,7 @@ using XMesh = Microsoft.Xna.Framework.Graphics.ModelMesh;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework.Graphics;
 using System.Linq;
+using XMatrix = Microsoft.Xna.Framework.Matrix;
 
 namespace ChamberLib
 {
@@ -116,6 +117,141 @@ namespace ChamberLib
                     destLight.SpecularColor = light.SpecularColor.ToXna();
 
                     effect2.LightingEnabled = true;
+                }
+            }
+        }
+
+        public void DisableDirectionalLight(int index)
+        {
+            foreach (var effect in GetEffects())
+            {
+                var effect2 = effect as Microsoft.Xna.Framework.Graphics.IEffectLights;
+                if (effect2 != null)
+                {
+                    Microsoft.Xna.Framework.Graphics.DirectionalLight destLight;
+                    if (index == 1)
+                    {
+                        effect2.DirectionalLight1.Enabled = false;
+                    }
+                    else if (index == 2)
+                    {
+                        effect2.DirectionalLight2.Enabled = false;
+                    }
+                    else
+                    {
+                        effect2.DirectionalLight0.Enabled = false;
+                    }
+                }
+            }
+        }
+
+        public void SetWorldViewProjection(Matrix world, Matrix view, Matrix projection)
+        {
+            var world2 = world.ToXna();
+            var view2 = view.ToXna();
+            var projection2 = projection.ToXna();
+
+            foreach (var effect in GetEffects())
+            {
+                var effect2 = effect as Microsoft.Xna.Framework.Graphics.IEffectMatrices;
+                if (effect2 != null)
+                {
+                    effect2.World = world2;
+                    effect2.View = view2;
+                    effect2.Projection = projection2;
+                }
+                else
+                {
+                    effect.Parameters["World"].SetValue(world2);
+                    effect.Parameters["View"].SetValue(view2);
+                    effect.Parameters["Projection"].SetValue(projection2);
+                }
+            }
+        }
+
+        public void SetAlpha(float alpha)
+        {
+            foreach (var effect in GetEffects())
+            {
+                var skinnedEffect = effect as SkinnedEffect;
+                var basicEffect = effect as BasicEffect;
+                if (skinnedEffect != null)
+                {
+                    skinnedEffect.Alpha = alpha;
+                }
+                else if (basicEffect != null)
+                {
+                    basicEffect.Alpha = alpha;
+                }
+            }
+        }
+
+        public void SetTexture(ITexture2D texture)
+        {
+            var texture2 = ((Texture2DAdapter)texture).Texture;
+            foreach (var effect in GetEffects())
+            {
+                var skinnedEffect = effect as SkinnedEffect;
+                var basicEffect = effect as BasicEffect;
+                if (skinnedEffect != null)
+                {
+                    skinnedEffect.Texture = texture2;
+                }
+                else if (basicEffect != null)
+                {
+                    basicEffect.Texture = texture2;
+                }
+            }
+        }
+
+        public void SetBoneTransforms(Matrix[] boneTransforms, float verticalOffset, Matrix world)
+        {
+            Matrix[] boneTransformsCopy = null;
+            XMatrix[] boneTransformsCopy2 = null;
+
+            if (boneTransforms != null)
+            {
+                boneTransformsCopy = boneTransforms.ToArray();
+                boneTransformsCopy[0] = Matrix.CreateTranslation(0, verticalOffset, 0) * boneTransformsCopy[0];
+                boneTransformsCopy2 = boneTransformsCopy.ToXna();
+            }
+
+            foreach (var mesh in this.GetMeshes())
+            {
+                foreach (Effect effect in ((MeshAdapter)mesh).Mesh.Effects)
+                {
+                    var skinnedEffect = effect as SkinnedEffect;
+                    var basicEffect = effect as BasicEffect;
+                    if (skinnedEffect != null)
+                    {
+                        if (boneTransforms != null)
+                        {
+                            skinnedEffect.SetBoneTransforms(boneTransformsCopy2);
+                        }
+                    }
+                    else if (basicEffect != null)
+                    {
+                        if (boneTransforms != null)
+                        {
+                            basicEffect.World = (boneTransforms[mesh.ParentBone.Index] * world).ToXna();
+                        }
+                        else
+                        {
+                            basicEffect.World = world.ToXna();
+                        }
+                    }
+                }
+            }
+        }
+
+        public void EnableDefaultLighting()
+        {
+            foreach (var effect in GetEffects())
+            {
+                var effect2 = effect as IEffectLights;
+                if (effect2 != null)
+                {
+                    effect2.EnableDefaultLighting();
                 }
             }
         }
