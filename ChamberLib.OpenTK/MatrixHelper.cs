19e70a5a2c19118bd1345df9ab78dabfc051960d
izrik <izrik@izrik.com>
2014-06-25 10:34:43 -0500

    Convert to and from OpenTK matrices.

---
+++ ChamberLib.OpenTK/MatrixHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ChamberLib
+{
+    public static class MatrixHelper
+    {
+        public static OpenTK.Matrix4 ToOpenTK(this ChamberLib.Matrix m)
+        {
+            return new OpenTK.Matrix4(
+                m.M11, m.M12, m.M13, m.M14,
+                m.M21, m.M22, m.M23, m.M24,
+                m.M31, m.M32, m.M33, m.M34,
+                m.M41, m.M42, m.M43, m.M44);
+        }
+
+        public static ChamberLib.Matrix ToChamber(this OpenTK.Matrix4 m)
+        {
+            return new ChamberLib.Matrix(
+                m.M11, m.M12, m.M13, m.M14,
+                m.M21, m.M22, m.M23, m.M24,
+                m.M31, m.M32, m.M33, m.M34,
+                m.M41, m.M42, m.M43, m.M44);
+        }
+    }
+}
+
