dcfa2dcd6ca98f807a6edf48e7e0a443ff7c6f02
izrik <izrik@izrik.com>
2014-06-25 10:51:51 -0500

    Partial implementation of MeasureString.

--- ChamberLib.OpenTK/FontAdapter.cs
+++ ChamberLib.OpenTK/FontAdapter.cs
@@ -8,9 +8,12 @@ namespace ChamberLib
         {
         }
 
+        public float CharacterWidth = 15;
+        public float CharacterHeight = 25;
+
         public Vector2 MeasureString(string text)
         {
-            return new Vector2(100, 25);
+            return new Vector2(text.Length * CharacterWidth, CharacterHeight);
         }
     }
 }
