69443a6dfae9fb37d7a58d006727cb091e15444c
izrik <izrik@izrik.com>
2015-03-03 08:05:02 -0600

    Provide a stock processor and importer.

---
+++ Content/ContentImporter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ChamberLib.Content
+{
+    public class ContentImporter : IContentImporter
+    {
+        public ContentImporter(
+            ModelImporter modelImporter,
+            TextureImporter textureImporter,
+            ShaderImporter shaderImporter,
+            FontImporter fontImporter,
+            SongImporter songImporter,
+            SoundEffectImporter soundEffectImporter)
+        {
+            ModelImporter = modelImporter;
+            TextureImporter = textureImporter;
+            ShaderImporter = shaderImporter;
+            FontImporter = fontImporter;
+            SongImporter = songImporter;
+            SoundEffectImporter = soundEffectImporter;
+        }
+
+        public readonly ModelImporter ModelImporter;
+        public readonly TextureImporter TextureImporter;
+        public readonly ShaderImporter ShaderImporter;
+        public readonly FontImporter FontImporter;
+        public readonly SongImporter SongImporter;
+        public readonly SoundEffectImporter SoundEffectImporter;
+
+        public ModelContent ImportModel(string name, IContentImporter importer = null)
+        {
+            return ModelImporter(name, importer);
+        }
+        public TextureContent ImportTexture2D(string name, IContentImporter importer = null)
+        {
+            return TextureImporter(name, importer);
+        }
+        public FontContent ImportFont(string name, IContentImporter importer = null)
+        {
+            return FontImporter(name, importer);
+        }
+        public SongContent ImportSong(string name, IContentImporter importer = null)
+        {
+            return SongImporter(name, importer);
+        }
+        public SoundEffectContent ImportSoundEffect(string name, IContentImporter importer = null)
+        {
+            return SoundEffectImporter(name, importer);
+        }
+        public ShaderContent ImportShader(string name, IContentImporter importer = null, object bindattrs = null)
+        {
+            return ShaderImporter(name, importer);
+        }
+    }
+}
+
---
+++ Content/ContentProcessor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ChamberLib.Content
+{
+    public class ContentProcessor : IContentProcessor
+    {
+        public ContentProcessor(
+            ModelProcessor modelProcessor,
+            TextureProcessor textureProcessor,
+            ShaderProcessor shaderProcessor,
+            FontProcessor fontProcessor,
+            SongProcessor songProcessor,
+            SoundEffectProcessor soundEffectProcessor)
+        {
+            ModelProcessor = modelProcessor;
+            TextureProcessor = textureProcessor;
+            ShaderProcessor = shaderProcessor;
+            FontProcessor = fontProcessor;
+            SongProcessor = songProcessor;
+            SoundEffectProcessor = soundEffectProcessor;
+        }
+
+        public readonly ModelProcessor ModelProcessor;
+        public readonly TextureProcessor TextureProcessor;
+        public readonly ShaderProcessor ShaderProcessor;
+        public readonly FontProcessor FontProcessor;
+        public readonly SongProcessor SongProcessor;
+        public readonly SoundEffectProcessor SoundEffectProcessor;
+
+        public IModel ProcessModel(ModelContent asset, IContentProcessor importer = null)
+        {
+            return ModelProcessor(asset, importer);
+        }
+        public ITexture2D ProcessTexture2D(TextureContent asset, IContentProcessor importer = null)
+        {
+            return TextureProcessor(asset, importer);
+        }
+        public IFont ProcessFont(FontContent asset, IContentProcessor importer = null)
+        {
+            return FontProcessor(asset, importer);
+        }
+        public ISong ProcessSong(SongContent asset, IContentProcessor importer = null)
+        {
+            return SongProcessor(asset, importer);
+        }
+        public ISoundEffect ProcessSoundEffect(SoundEffectContent asset, IContentProcessor importer = null)
+        {
+            return SoundEffectProcessor(asset, importer);
+        }
+        public IShader ProcessShader(ShaderContent asset, IContentProcessor importer = null, object bindattrs = null)
+        {
+            return ShaderProcessor(asset, importer);
+        }
+    }
+}
+
