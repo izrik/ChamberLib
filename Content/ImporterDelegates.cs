90551cd1cf9ba28f2efb0cd19bfb44fca751e388
izrik <izrik@izrik.com>
2015-03-03 07:53:56 -0600

    Specific processor and importer delegates.

---
+++ Content/ImporterDelegates.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChamberLib.Content
+{
+    public delegate ModelContent ModelImporter(string filename, IContentImporter importer);
+    public delegate TextureContent TextureImporter(string filename, IContentImporter importer);
+    public delegate ShaderContent ShaderImporter(string filename, IContentImporter importer);
+    public delegate FontContent FontImporter(string filename, IContentImporter importer);
+    public delegate SongContent SongImporter(string filename, IContentImporter importer);
+    public delegate SoundEffectContent SoundEffectImporter(string filename, IContentImporter importer);
+}
+
---
+++ Content/ProcessorDelegates.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChamberLib.Content
+{
+    public delegate IModel ModelProcessor(ModelContent asset, IContentProcessor processor);
+    public delegate ITexture2D TextureProcessor(TextureContent asset, IContentProcessor processor);
+    public delegate IShader ShaderProcessor(ShaderContent asset, IContentProcessor processor);
+    public delegate IFont FontProcessor(FontContent asset, IContentProcessor processor);
+    public delegate ISong SongProcessor(SongContent asset, IContentProcessor processor);
+    public delegate ISoundEffect SoundEffectProcessor(SoundEffectContent asset, IContentProcessor processor);
+}
+
