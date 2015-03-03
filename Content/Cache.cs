0e6581c003e521959ed81c3f3272da029a9a0a5b
izrik <izrik@izrik.com>
2015-03-03 10:37:45 -0600

    Add additional cache classes for different numbers of parameters.

--- Content/Cache.cs
+++ Content/Cache.cs
@@ -3,6 +3,30 @@ using System.Collections.Generic;
 
 namespace ChamberLib
 {
+    public class Cache<TInput, TOutput>
+    {
+        public Cache(Func<TInput, TOutput> next)
+        {
+            if (next == null) throw new ArgumentNullException("next");
+
+            this.next = next;
+        }
+
+        readonly Func<TInput, TOutput> next;
+
+        readonly Dictionary<TInput, TOutput> cache = new Dictionary<TInput, TOutput>();
+
+        public TOutput Call(TInput input)
+        {
+            if (cache.ContainsKey(input))
+                return cache[input];
+
+            var output = next(input);
+            cache[input] = output;
+            return output;
+        }
+    }
+
     public class Cache<TInput, TContent, TOutput>
     {
         public Cache(Func<TInput, TContent, TOutput> next)
@@ -22,6 +46,30 @@ namespace ChamberLib
                 return cache[input];
 
             var output = next(input, content);
+            cache[input] = output;
+            return output;
+        }
+    }
+
+    public class Cache<TInput, TContent, TParam, TOutput>
+    {
+        public Cache(Func<TInput, TContent, TParam, TOutput> next)
+        {
+            if (next == null) throw new ArgumentNullException("next");
+
+            this.next = next;
+        }
+
+        readonly Func<TInput, TContent, TParam, TOutput> next;
+
+        readonly Dictionary<TInput, TOutput> cache = new Dictionary<TInput, TOutput>();
+
+        public TOutput Call(TInput input, TContent content, TParam param)
+        {
+            if (cache.ContainsKey(input))
+                return cache[input];
+
+            var output = next(input, content, param);
             cache[input] = output;
             return output;
         }
