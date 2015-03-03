5f0d03e57d37134e63bbca24751514c7a7f61f4e
izrik <izrik@izrik.com>
2015-03-03 14:23:16 -0600

    Extract the SoundEffectImporter class.

---
+++ ChamberLib.OpenTK/SoundEffectImporter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using FileFormat = ChamberLib.SoundEffect.FileFormat;
+using NAudio.Wave;
+using NVorbis;
+using System.Diagnostics;
+
+namespace ChamberLib
+{
+    public class SoundEffectImporter
+    {
+        public SoundEffectContent ImportSoundEffect(string name, Stream stream, FileFormat fileFormat)
+        {
+            if (stream == null) throw new ArgumentNullException("stream");
+
+            int numChannels;
+            int bitsPerSample;
+            int samplesPerSecond;
+            byte [] audioData;
+
+            switch (fileFormat)
+            {
+            case FileFormat.Wav:
+                var wfr = new WaveFileReader(stream);
+
+                numChannels = wfr.WaveFormat.Channels;
+                bitsPerSample = wfr.WaveFormat.BitsPerSample;
+                samplesPerSecond = wfr.WaveFormat.SampleRate;
+
+                if (numChannels != 1 && numChannels != 2) throw new NotSupportedException("The sound format is not supported");
+                if (bitsPerSample != 8 && bitsPerSample != 16) throw new NotSupportedException("The sound format is not supported");
+
+                int length = (int)(wfr.BlockAlign * (wfr.Length / wfr.BlockAlign)); //TODO: length might be more than MAX_INT
+                audioData = new byte[length];
+                wfr.Read(audioData, 0, length);
+
+                return new SoundEffectContent(name, numChannels, bitsPerSample,
+                    samplesPerSecond, audioData);
+
+            case FileFormat.Ogg:
+                var reader = new VorbisReader(stream, false);
+
+                numChannels = reader.Channels;
+                bitsPerSample = 16;
+                samplesPerSecond = reader.SampleRate;
+
+                if (numChannels != 1 && numChannels != 2) throw new NotSupportedException("The sound format is not supported");
+                if (bitsPerSample != 8 && bitsPerSample != 16) throw new NotSupportedException("The sound format is not supported");
+
+                var numSamples = (int)(reader.TotalSamples * numChannels); //TODO: samples might be more than MAX_INT
+                float[] buffer1 = new float[numSamples];
+                int time1 = Environment.TickCount;
+                int numSamplesRead = reader.ReadSamples(buffer1, 0, numSamples);
+                int time2 = Environment.TickCount;
+                if (numSamplesRead != numSamples)
+                {
+                    Debug.WriteLine("numSamplesRead does not match numSamples - {0}", name);
+                }
+
+                audioData = new byte[numSamples * 2];
+                // convert from float to short
+                int i;
+                for (i = 0; i < numSamples; i++)
+                {
+                    var sample = (short)Math.Max(Math.Min((int)(buffer1[i] * 32767f), short.MaxValue), short.MinValue);
+                    audioData[2 * i + 0] = (byte)(sample & 0xff);
+                    audioData[2 * i + 1] = (byte)((sample & 0xff00) >> 8);
+                }
+                int time3 = Environment.TickCount;
+                return new SoundEffectContent(name, numChannels, bitsPerSample,
+                    samplesPerSecond, audioData);
+
+            default:
+                throw new IOException(string.Format("The stream \"{0}\" is of an unknown type, \"{1}\"", name, fileFormat));
+
+            }
+        }
+    }
+}
+
