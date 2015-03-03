3b25ce222ae730f8fc34cd0101a4f273dd69cbd0
izrik <izrik@izrik.com>
2015-03-03 08:09:28 -0600

    A basic caching mechanism.

---
+++ Content/Cache.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChamberLib
+{
+    public class Cache<TInput, TContent, TOutput>
+    {
+        public Cache(Func<TInput, TContent, TOutput> next)
+        {
+            if (next == null) throw new ArgumentNullException("next");
+
+            this.next = next;
+        }
+
+        readonly Func<TInput, TContent, TOutput> next;
+
+        readonly Dictionary<TInput, TOutput> cache = new Dictionary<TInput, TOutput>();
+
+        public TOutput Call(TInput input, TContent content)
+        {
+            if (cache.ContainsKey(input))
+                return cache[input];
+
+            var output = next(input, content);
+            cache[input] = output;
+            return output;
+        }
+    }
+}
+
