85baf0094c0d5dabf6907a691b2344ba106c1619
izrik <izrik@izrik.com>
2015-03-04 07:45:41 -0600

    Resolve filenames before importing.

---
+++ Content/ResolvingContentImporter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace ChamberLib.Content
+{
+    public class ResolvingContentImporter : IContentImporter
+    {
+        public ResolvingContentImporter(IContentImporter next, string basePath=null)
+        {
+            if (next == null) throw new ArgumentNullException("next");
+
+            this.next = next;
+
+            if (!string.IsNullOrWhiteSpace(basePath))
+                BasePath = basePath;
+        }
+
+        public string BasePath = "";
+        readonly IContentImporter next;
+
+        public ModelContent ImportModel(string name, IContentImporter importer = null)
+        {
+            if (!string.IsNullOrEmpty(BasePath))
+                name = Path.Combine(BasePath, name);
+
+            return next.ImportModel(name, importer);
+        }
+
+        public TextureContent ImportTexture2D(string name, IContentImporter importer = null)
+        {
+            if (!string.IsNullOrEmpty(BasePath))
+                name = Path.Combine(BasePath, name);
+
+            return next.ImportTexture2D(name, importer);
+        }
+
+        public FontContent ImportFont(string name, IContentImporter importer = null)
+        {
+            if (!string.IsNullOrEmpty(BasePath))
+                name = Path.Combine(BasePath, name);
+
+            return next.ImportFont(name, importer);
+        }
+
+        public SongContent ImportSong(string name, IContentImporter importer = null)
+        {
+            if (!string.IsNullOrEmpty(BasePath))
+                name = Path.Combine(BasePath, name);
+
+            return next.ImportSong(name, importer);
+        }
+
+        public SoundEffectContent ImportSoundEffect(string name, IContentImporter importer = null)
+        {
+            if (!string.IsNullOrEmpty(BasePath))
+                name = Path.Combine(BasePath, name);
+
+            return next.ImportSoundEffect(name, importer);
+        }
+
+        public ShaderContent ImportShader(string name, IContentImporter importer = null)
+        {
+            if (!string.IsNullOrEmpty(BasePath))
+                name = Path.Combine(BasePath, name);
+
+            return next.ImportShader(name, importer);
+        }
+    }
+}
+
