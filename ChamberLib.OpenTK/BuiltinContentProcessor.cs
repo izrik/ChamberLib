01d49b850da8ae25ff91345f99d73b6d8dc16cfc
izrik <izrik@izrik.com>
2015-03-04 14:31:05 -0600

    Create a processor class to 'process' fonts and the built-in shaders.

---
+++ ChamberLib.OpenTK/BuiltinContentProcessor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ChamberLib.Content;
+
+namespace ChamberLib
+{
+    public class BuiltinContentProcessor : IContentProcessor
+    {
+        public BuiltinContentProcessor(IContentProcessor next)
+        {
+            if (next == null) throw new ArgumentNullException("next");
+
+            this.next = next;
+        }
+
+        readonly IContentProcessor next;
+
+        public IModel ProcessModel(ModelContent asset, IContentProcessor processor = null)
+        {
+            return next.ProcessModel(asset, processor);
+        }
+        public ITexture2D ProcessTexture2D(TextureContent asset, IContentProcessor processor = null)
+        {
+            return next.ProcessTexture2D(asset, processor);
+        }
+        public IFont ProcessFont(FontContent asset, IContentProcessor processor = null)
+        {
+            return new FontAdapter(asset);
+        }
+        public ISong ProcessSong(SongContent asset, IContentProcessor processor = null)
+        {
+            return next.ProcessSong(asset, processor);
+        }
+        public ISoundEffect ProcessSoundEffect(SoundEffectContent asset, IContentProcessor processor = null)
+        {
+            return next.ProcessSoundEffect(asset, processor);
+        }
+        public IShader ProcessShader(ShaderContent asset, IContentProcessor processor = null, object bindattrs=null)
+        {
+            if (asset == BuiltinShaders.BasicShaderContent)
+                return BuiltinShaders.BasicShader;
+
+            if (asset == BuiltinShaders.SkinnedShaderContent)
+                return BuiltinShaders.SkinnedShader;
+
+            return next.ProcessShader(asset, processor, bindattrs);
+        }
+    }
+}
+
