6675d3bdd514d105a61aca9bb228db488fd52b91
izrik <izrik@izrik.com>
2015-05-19 07:57:26 -0500

    Add a way to choose which importer to use based on the file's entension.

---
+++ Content/ChooseByExtensionImporter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using ChamberLib.Content;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace ChamberLib.Content
+{
+    public delegate U Importer<U>(string filename, IContentImporter importer);
+
+    public class ChooseByExtensionImporter<T>
+    {
+
+        public ChooseByExtensionImporter(IDictionary<string, Importer<T>> importersByExtension, Importer<T> next=null)
+        {
+            _importersByExtension = new Dictionary<string, Importer<T>>(importersByExtension);
+            _next = next;
+        }
+
+        readonly Dictionary<string, Importer<T>> _importersByExtension;
+        readonly Importer<T> _next;
+
+        public T Import(string filename, IContentImporter importer)
+        {
+            var ext = Path.GetExtension(filename);
+            while (ext.StartsWith("."))
+            {
+                ext = ext.Substring(1);
+            }
+
+            if (_importersByExtension.ContainsKey(ext))
+            {
+                return _importersByExtension[ext](filename, importer);
+            }
+
+            if (_next != null)
+            {
+                return _next(filename, importer);
+            }
+
+            throw new NotImplementedException(
+                string.Format(
+                    "No importer specified for extension \"{0}\"", ext));
+        }
+    }
+}
+
