a92f173428d545aa1001ae078b512f97f80d3a2b
izrik <izrik@izrik.com>
2015-05-19 11:07:11 -0500

    Prep for release.

---
+++ ChamberLib.OpenTK/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ChamberLib.OpenTK")]
+[assembly: AssemblyDescription("OpenTK bindings for ChamberLib")]
+[assembly: AssemblyCompany("Metaphysics Industries, Inc.")]
+[assembly: AssemblyCopyright("Copyright 2015 izrik and Metaphysics Industries, Inc.")]
+
+[assembly: AssemblyVersion("0.3")]
