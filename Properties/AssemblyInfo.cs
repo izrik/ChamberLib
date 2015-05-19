6d41234e5a73603db0a1a01f5059caf0159362e7
izrik <izrik@izrik.com>
2015-05-19 11:46:53 -0500

    Use the packaged ChamberLib.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,14 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("ChamberLib")]
 [assembly: AssemblyDescription("A cross-platform library to make 3D graphics a little easier.")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Metaphysics Industries, Inc.")]
-[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Copyright 2015 izrik and Metaphysics Industries, Inc.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: AssemblyVersion("0.3")]
 
