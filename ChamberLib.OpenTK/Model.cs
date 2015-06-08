65ac1582e80088a1c272145769266afe90d3c566
izrik <izrik@izrik.com>
2015-06-08 08:16:47 -0500

    Apply to all materials and shaders.

--- ChamberLib.OpenTK/Model.cs
+++ ChamberLib.OpenTK/Model.cs
@@ -150,16 +150,11 @@ namespace ChamberLib.OpenTK
 
             foreach (var material in GetAllMaterials())
             {
-                if (material.Shader != BuiltinShaders.BasicShader)
+                int i;
+                for (i = 0; i < 32 && i < boneTransforms.Length; i++)
                 {
-                    int i;
-                    for (i = 0; i < 32 && i < boneTransforms.Length; i++)
-                    {
-                        var name = string.Format("bones[{0}]", i);
-                        material.Shader2.SetUniform(name, boneTransforms[i]);
-                    }
-
-                    break;
+                    var name = string.Format("bones[{0}]", i);
+                    material.Shader2.SetUniform(name, boneTransforms[i]);
                 }
             }
         }
