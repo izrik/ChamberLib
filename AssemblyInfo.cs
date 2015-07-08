a7852f31a6789fecb10df10fdecb67d677603fd4
izrik <izrik@izrik.com>
2015-07-08 07:31:06 -0500

    Release

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyCompany("Metaphysics Industries, Inc.")]
 [assembly: AssemblyCopyright("Copyright 2015 izrik and Metaphysics Industries, Inc.")]
 
-[assembly: AssemblyVersion("0.7")]
+[assembly: AssemblyVersion("0.8")]
 
