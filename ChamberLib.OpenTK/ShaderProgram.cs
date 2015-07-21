72005845b163490362f829560ca1259c8f302232
izrik <izrik@izrik.com>
2015-07-21 08:22:29 -0500

    Move the ShaderStage class to its own file.

--- ChamberLib.OpenTK/ShaderProgram.cs
+++ ChamberLib.OpenTK/ShaderProgram.cs
@@ -60,39 +60,7 @@ namespace ChamberLib.OpenTK
             }
         }
 
-        public class ShaderStage : IShaderStage
-        {
-            public ShaderStage(string source, ShaderType shaderType)
-            {
-                Source = source;
-                ShaderType = shaderType;
-            }
 
-            public int ShaderID { get; protected set; }
-            public string Source { get; protected set; }
-            public ShaderType ShaderType { get; protected set; }
-
-            public void MakeReady()
-            {
-                if (ShaderID != 0) return;
-
-                GLHelper.CheckError();
-                ShaderID = GL.CreateShader(ShaderType.ToOpenTK());
-                GLHelper.CheckError();
-                GL.ShaderSource(ShaderID, Source);
-                GLHelper.CheckError();
-                GL.CompileShader(ShaderID);
-                GLHelper.CheckError();
-
-                int result;
-                GL.GetShader(ShaderID, ShaderParameter.CompileStatus, out result);
-                Debug.WriteLine("{1} compile status: {0}", result, ShaderType);
-                GLHelper.CheckError();
-                Debug.WriteLine("{0} info:", ShaderType);
-                Debug.WriteLine(GL.GetShaderInfoLog(ShaderID));
-                GLHelper.CheckError();
-            }
-        }
 
         public ShaderStage VertexShader;
         public ShaderStage FragmentShader;
