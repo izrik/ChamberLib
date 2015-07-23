a7d8d6938f867a134f6638248aea4b481163999b
izrik <izrik@izrik.com>
2015-07-23 07:52:51 -0500

    Remove the unused bindattrs parameter from the ShaderStageProcessor delegate.

--- Content/ProcessorDelegates.cs
+++ Content/ProcessorDelegates.cs
@@ -4,7 +4,7 @@ namespace ChamberLib.Content
 {
     public delegate IModel ModelProcessor(ModelContent asset, IContentProcessor processor);
     public delegate ITexture2D TextureProcessor(TextureContent asset, IContentProcessor processor);
-    public delegate IShaderStage ShaderStageProcessor(ShaderContent asset, IContentProcessor processor, object bindattrs=null);
+    public delegate IShaderStage ShaderStageProcessor(ShaderContent asset, IContentProcessor processor);
     public delegate IFont FontProcessor(FontContent asset, IContentProcessor processor);
     public delegate ISong SongProcessor(SongContent asset, IContentProcessor processor);
     public delegate ISoundEffect SoundEffectProcessor(SoundEffectContent asset, IContentProcessor processor);
