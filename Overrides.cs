8fa3c6f237848c517f290c57ea0c4962d66f159f
izrik <izrik@izrik.com>
2015-07-24 08:53:21 -0500

    Create the Overrides class, for overriding things when rendering.

---
+++ Overrides.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChamberLib
+{
+    public class Overrides
+    {
+        public LightingData? Lighting;
+        public IMaterial Material;
+        public IShaderProgram ShaderProgram;
+        public IShaderStage VertexShader;
+        public IShaderStage FragmentShader;
+    }
+}
