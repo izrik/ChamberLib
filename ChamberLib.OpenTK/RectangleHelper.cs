83a620cac7ef8005214e15296f93ae4bffcae5c0
izrik <izrik@izrik.com>
2015-11-27 08:52:40 -0600

    Convert OpenTK/WinForms rectangles to ChamberLib.

---
+++ ChamberLib.OpenTK/RectangleHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChamberLib.OpenTK
+{
+    public static class RectangleHelper
+    {
+        public static RectangleI ToChamber(this System.Drawing.Rectangle rect)
+        {
+            return new RectangleI(rect.Left, rect.Top, rect.Width, rect.Height);
+        }
+    }
+}
