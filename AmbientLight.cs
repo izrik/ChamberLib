90c40f9b9748e8557a8ff3c2c128f0324005c4ce
izrik <izrik@izrik.com>
2020-02-28 14:31:56 -0600

    Add an AmbientLight class.

---
+++ AmbientLight.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ChamberLib
+{
+    public class AmbientLight
+    {
+        public AmbientLight(Vector3 color=default(Vector3))
+        {
+            Color = color;
+        }
+
+        public Vector3 Color { get; set; }
+    }
+}
