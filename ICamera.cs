8ee9a5170ade6f808dc9befb4adc65200225a084
izrik <izrik@izrik.com>
2020-02-29 16:39:20 -0600

    Add the ICamera interface.

---
+++ ICamera.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ChamberLib
+{
+    public interface ICamera
+    {
+        Matrix View { get; }
+
+        Matrix Projection { get; }
+        Viewport Viewport { get; }
+
+        Vector3 Project(Vector3 source, Matrix world);
+        Vector3 Unproject(Vector3 source, Matrix world);
+    }
+}
