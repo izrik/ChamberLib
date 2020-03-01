58c230b987085eeb840c836838848a64340a0713
izrik <izrik@izrik.com>
2020-02-29 22:42:16 -0600

    Add the ViewportSource class.

---
+++ ViewportSource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ChamberLib
+{
+    public class ViewportSource
+    {
+        public ViewportSource(Func<Viewport> func)
+        {
+            _func = func;
+        }
+
+        Func<Viewport> _func;
+
+        public Viewport Viewport
+        {
+            get { return _func(); }
+        }
+
+    }
+}
