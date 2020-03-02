242b0e318af4b80fc05cfbe9c3de114f63c0bb27
izrik <izrik@izrik.com>
2020-03-02 12:09:26 -0600

    Remove the Project and Unproject methods from cameras.

--- ICamera.cs
+++ ICamera.cs
@@ -8,8 +8,5 @@ namespace ChamberLib
 
         Matrix Projection { get; }
         Viewport Viewport { get; }
-
-        Vector3 Project(Vector3 source, Matrix world);
-        Vector3 Unproject(Vector3 source, Matrix world);
     }
 }
