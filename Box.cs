d0ce401aefb4e3805794ce1348a2ab790707f3cf
izrik <izrik@izrik.com>
2020-03-02 12:05:33 -0600

    Replace the ViewportSource class with a generic Box<T>.

---
+++ Box.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ChamberLib
+{
+    public class Box<T>
+        where T : struct
+    {
+        public Box(T initialValue=default(T))
+        {
+            Value = initialValue;
+        }
+
+        public T Value { get; set; }
+    }
+}
