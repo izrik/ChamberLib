bdd34546e0cd1358a568d33cf4421d5ba67ddba2
izrik <izrik@izrik.com>
2020-03-03 14:03:59 -0600

    Re-format.

--- ChamberLibTests/Vector3Test.cs
+++ ChamberLibTests/Vector3Test.cs
@@ -8,13 +8,13 @@ namespace ChamberLibTests
     public class Vector3Test
     {
         [Test]
-        [TestCase(1, 0, 0,   0, 1, 0,  0, 0, 1 )]
-        [TestCase(0, 1, 0,   0, 0, 1,  1, 0, 0 )]
-        [TestCase(0, 0, 1,   1, 0, 0,  0, 1, 0 )]
-        [TestCase(-1, 0, 0,   0, 1, 0,  0, 0, -1 )]
-        [TestCase(0, -1, 0,   0, 0, 1,  -1, 0, 0 )]
-        [TestCase(0, 0, -1,   1, 0, 0,  0, -1, 0 )]
-        [TestCase(2, 0, 0,   0, 3, 0,  0, 0, 6 )]
+        [TestCase(1, 0, 0, 0, 1, 0, 0, 0, 1)]
+        [TestCase(0, 1, 0, 0, 0, 1, 1, 0, 0)]
+        [TestCase(0, 0, 1, 1, 0, 0, 0, 1, 0)]
+        [TestCase(-1, 0, 0, 0, 1, 0, 0, 0, -1)]
+        [TestCase(0, -1, 0, 0, 0, 1, -1, 0, 0)]
+        [TestCase(0, 0, -1, 1, 0, 0, 0, -1, 0)]
+        [TestCase(2, 0, 0, 0, 3, 0, 0, 0, 6)]
         public void Vector3CrossTest(float ax, float ay, float az, float bx, float by, float bz, float ex, float ey, float ez)
         {
             var a = new Vector3(ax, ay, az);
