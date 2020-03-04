9cbf756849f44362663fbf94df9a91cdbc8bfd90
izrik <izrik@izrik.com>
2020-03-04 14:22:05 -0600

    Allow for overriding a 'vertex material'.

--- Overrides.cs
+++ Overrides.cs
@@ -9,10 +9,12 @@ namespace ChamberLib
     {
         public Overrides(
             IMaterial material=null,
+            IMaterial vertexMaterial=null,
             float? alpha=null,
             ShaderUniforms uniforms=null)
         {
             this.Material = material;
+            this.VertexMaterial = vertexMaterial;
             this.Alpha = alpha;
             this.Uniforms = uniforms;
         }
@@ -20,11 +22,13 @@ namespace ChamberLib
         public static Overrides FromPrototype(
             Overrides prototype,
             IMaterial material = null,
+            IMaterial vertexMaterial = null,
             float? alpha = null,
             ShaderUniforms uniforms = null)
         {
             return new Overrides(
                 material: material ?? prototype.Material,
+                vertexMaterial: vertexMaterial ?? prototype.VertexMaterial,
                 alpha: alpha ?? prototype.Alpha,
                 uniforms: uniforms ?? prototype.Uniforms);
         }
@@ -33,6 +37,11 @@ namespace ChamberLib
         public IMaterial GetMaterial(IMaterial defaultValue)
         {
             return this.Material ?? defaultValue;
+        }
+        public IMaterial VertexMaterial;
+        public IMaterial GetVertexMaterial(IMaterial defaultValue)
+        {
+            return this.VertexMaterial ?? defaultValue;
         }
         public float? Alpha;
         public float GetAlpha(float defaultValue)
