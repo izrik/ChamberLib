02d818f053e7e3a94c86e2eb565fdc47d980c08c
izrik <izrik@izrik.com>
2020-03-04 16:13:52 -0600

    Split out two material classes, one vertex, one fragment.

---
+++ ChamberLib.OpenTK/FragmentMaterial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ChamberLib.Content;
+
+namespace ChamberLib.OpenTK
+{
+    public class FragmentMaterial : Material
+    {
+        public FragmentMaterial(FragmentMaterialContent material, ContentResolver resolver, IContentProcessor processor)
+            : base(material, resolver, processor)
+        {
+        }
+    }
+}
---
+++ ChamberLib.OpenTK/VertexMaterial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ChamberLib.Content;
+
+namespace ChamberLib.OpenTK
+{
+    public class VertexMaterial : Material
+    {
+        public VertexMaterial(VertexMaterialContent material, ContentResolver resolver, IContentProcessor processor)
+            : base(material, resolver, processor)
+        {
+        }
+    }
+}
