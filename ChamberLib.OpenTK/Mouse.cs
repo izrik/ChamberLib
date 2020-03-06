a9d000fe7ab378bea9c0d041b02b57bcb2f0bbf5
izrik <izrik@izrik.com>
2020-03-06 07:55:03 -0600

    Add mouse classes.

---
+++ ChamberLib.OpenTK/Mouse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ChamberLib.OpenTK
+{
+    public class Mouse : IMouse
+    {
+        public Mouse()
+        {
+        }
+
+        public MouseState GetState()
+        {
+            return new MouseState();
+        }
+    }
+}
---
+++ MouseState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ChamberLib
+{
+    public struct MouseState
+    {
+        public MouseState(int x, int y, bool leftButton, bool middleButton,
+            bool rightButton, int wheel)
+        {
+            X = x;
+            Y = y;
+            LeftButton = leftButton;
+            MiddleButton = middleButton;
+            RightButton = rightButton;
+            Wheel = wheel;
+        }
+
+        public readonly int X;
+        public readonly int Y;
+        public readonly bool LeftButton;
+        public readonly bool MiddleButton;
+        public readonly bool RightButton;
+        public readonly int Wheel;
+    }
+}
