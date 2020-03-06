541a49c2f71aaad029fa1b26004b27c1fdb857c7
izrik <izrik@izrik.com>
2020-03-06 08:01:04 -0600

    Get the state of the mouse.

--- ChamberLib.OpenTK/Mouse.cs
+++ ChamberLib.OpenTK/Mouse.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using _OpenTK = global::OpenTK;
+using _OpenTKI = global::OpenTK.Input;
+
 namespace ChamberLib.OpenTK
 {
     public class Mouse : IMouse
     {
-        public Mouse()
-        {
-        }
-
         public MouseState GetState()
         {
-            return new MouseState();
+            var cstate = _OpenTKI.Mouse.GetCursorState();
+
+            return new MouseState(x: cstate.X, y: cstate.Y,
+                leftButton: cstate.IsButtonDown(_OpenTKI.MouseButton.Left),
+                rightButton: cstate.IsButtonDown(_OpenTKI.MouseButton.Right),
+                middleButton: cstate.IsButtonDown(_OpenTKI.MouseButton.Middle),
+                wheel: cstate.Wheel);
         }
     }
 }
