bd71ea389c3670a1d07207905c8f932dbc4ab232
izrik <izrik@izrik.com>
2020-03-26 18:06:30 -0500

    Pass a clear color for the render target.

--- IRenderTarget.cs
+++ IRenderTarget.cs
@@ -8,6 +8,8 @@ namespace ChamberLib
         int Height { get; }
 
         void Apply();
+        void Apply(Color clearColor);
+        void Apply(ColorF clearColor);
         void UnApply();
 
         ITexture2D AsTexture();
